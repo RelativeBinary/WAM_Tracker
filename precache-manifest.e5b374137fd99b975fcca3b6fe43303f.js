c6b63637a9083d1d7f193f861443fa0d0bcb2aa7
RelativeBinary <patrick.bornay@gmail.com>
2020-04-02 11:35:13 +1100

    Updates

---
+++ precache-manifest.e5b374137fd99b975fcca3b6fe43303f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32626c672b9cd4bb19aec78c374c20e",
+    "url": "/WAM_Tracker/index.html"
+  },
+  {
+    "revision": "9aca81c893868e553545",
+    "url": "/WAM_Tracker/static/css/main.1dea60de.chunk.css"
+  },
+  {
+    "revision": "77d4089d22cb2206d9b4",
+    "url": "/WAM_Tracker/static/js/2.fea59ed2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/WAM_Tracker/static/js/2.fea59ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aca81c893868e553545",
+    "url": "/WAM_Tracker/static/js/main.029543e4.chunk.js"
+  },
+  {
+    "revision": "e5f761f4173d59ec4a9a",
+    "url": "/WAM_Tracker/static/js/runtime-main.64315702.js"
+  }
+]);
